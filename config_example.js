5626a9b7deb021cd0dd0d367ccdf0c9c658fd08f
Amy Chan <chamy09@gmail.com>
2019-09-29 23:57:19 -0500

    Initial commit,  add spotify authentication

---
+++ config_example.js
@@ -0,0 +1,6 @@
+//Replace with your credentials and rename to config.js
+
+//spotify credentials
+process.env.CLIENT_ID = 'client_id';
+process.env.CLIENT_SECRET = 'client_secret';
+process.env.REDIRECT_URI = 'redirect_uri';
